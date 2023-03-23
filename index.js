048fb177f25fe93db9e6e452e53383da62297b5a
Four Source Technologies <118180580+4SourceTechnlogies@users.noreply.github.com>
2023-03-23 16:03:52 +0530

    added animation

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+AOS.init();
 
-var slide = [$('.carousel-item')];
-var sliderCards = $('.carousel-item .card')
+const parallax = document.getElementById("parallax");
 
-slide.forEach(element => {
-    $(sliderCards[1]).addClass('elevate')
+// Parallax Effect for DIV 1
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+  // DIV 1 background will move slower than other elements on scroll.
+});
+
+window.addEventListener("scroll", function () {
+  var headerTop = document.querySelector(".topNav");
+  var nav = this.document.querySelector(".stricky-header");
+  var bNav = this.document.querySelector('.bottomNav')
+  headerTop.classList.toggle(".htAnimate", window.scrollY > 0);
+  // nav.classList.toggle("stricky-fixed", window.scrollY > 1);
+  bNav.classList.toggle('fix', this.window.scrollY > 1)
 });
